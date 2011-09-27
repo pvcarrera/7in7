b5df8fc25cf17541ed8fb1c0fab48d9188eefd54
pvcarrera <vicapa99@gmail.com>
2011-09-27 23:49:08 +0200

    Basic problems

---
+++ firstDay.rb
@@ -0,0 +1,20 @@
+#Print the string "Hello, wold"
+puts 'Hello, wolrd'
+
+#For the string "Hello, Ruby", find the index of the word "Ruby"
+hello = 'Hello, Ruby'
+puts hello.index('Ruby')
+
+#Print your name ten times
+i = 0
+while i < 10
+	puts 'pablo' 
+	i = i + 1
+end
+
+#Print the string "This is sentence number 1" where the number 1 changes from 1 to 10
+i = 1
+while i < 11
+	puts "This is sentence number #{i}"
+	i = i + 1
+end
