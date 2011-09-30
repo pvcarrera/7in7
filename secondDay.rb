3246bd78cee3d4a3bc93bbb6437e8b89bd363f98
pvcarrera <pvicente@agil-az.com>
2011-09-30 13:45:59 +0200

    Array to hash and hash to array

--- secondDay.rb
+++ secondDay.rb
@@ -8,3 +8,17 @@ file = File.open("foo.txt","w")
 file << "Tedious mode"
 file.close
 
+#Convert array to hash
+a = ['uno', 1, 'dos', 2]
+Hash[*a]
+
+#Convert hash to array
+hash = {1 => 'uno', 2 => 'dos'}
+hash.to_a.flatten
+
+#iterate through a hash
+
+hash.each do |k,v|
+	puts "Clave = #{k}"
+	puts "Valor = #{v}"
+end
